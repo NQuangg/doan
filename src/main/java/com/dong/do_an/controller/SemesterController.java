5bd4e117ead1c853cb7b409ce291a29f89b4ad95
Trần Ngọc Quang <49584153+NQuangg@users.noreply.github.com>
2024-03-23 03:24:34 +0700

    add swagger

--- src/main/java/com/dong/do_an/controller/SemesterController.java
+++ src/main/java/com/dong/do_an/controller/SemesterController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("subject")
+@RequestMapping("semester")
 @RequiredArgsConstructor
 public class SemesterController {
 
--- src/main/java/com/dong/do_an/security/SecurityConfiguration.java
+++ src/main/java/com/dong/do_an/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
@@ -25,7 +27,10 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         registry -> registry
-                                .requestMatchers("/auth/login", "/auth/reset_password", "/file")
+                                .requestMatchers(
+                                        "/auth/login", "/auth/reset_password", "/file",
+                                        "/v3/**", "/swagger-ui/**"
+                                )
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
